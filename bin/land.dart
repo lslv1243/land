9f0445033c8b78dddf007bcfd23bdba569616e21
Leonardo da Silva <lslv1243@gmail.com>
2022-03-13 14:53:31 -0300

    created a test plural modifier

--- bin/land.dart
+++ bin/land.dart
@@ -4,22 +4,55 @@ void main(List<String> arguments) {
   final parser = Parser();
   final printer = PrintVisitor();
   final expression = parser.parse('''
-'You have {count,plural, 
+You have {count,plural, 
   =0{no items} 
   =1{1 item} 
   other{{count} items}
-}.'
+}.
 ''');
   expression.visit(printer);
 
-  final applier = ApplyVisitor({'count': '=1'});
+  final applier = ApplyVisitor(
+    {'count': 5},
+    modifiers: {
+      'plural': ApplyVisitorModifierPlural(),
+    },
+  );
   print(expression.visit(applier));
+}
+
+abstract class ApplyVisitorModifier {
+  String resolve(MultipleExpression expression, ApplyVisitor visitor);
+}
+
+class ApplyVisitorModifierPlural implements ApplyVisitorModifier {
+  @override
+  String resolve(MultipleExpression expression, ApplyVisitor visitor) {
+    final count = visitor.parameters[expression.parameter] as num;
+    final countString = count.toString();
+    for (final option in expression.options.entries) {
+      if (option.key[0] != '=') continue;
+      if (option.key.substring(1) == countString) {
+        return option.value.visit(visitor);
+      }
+    }
+    final plural = choosePlural(count, expression.options.keys.toSet());
+    return expression.options[plural]!.visit(visitor);
+  }
+
+  String choosePlural(num count, Set<String> options) {
+    // TODO: this is language specific
+    if (count == 1 && options.contains('one')) return 'one';
+    if (count == 2 && options.contains('two')) return 'two';
+    return 'other';
+  }
 }
 
 class ApplyVisitor implements ExpressionVisitor<String> {
   final Map<String, Object> parameters;
+  final Map<String, ApplyVisitorModifier> modifiers;
 
-  ApplyVisitor(this.parameters);
+  ApplyVisitor(this.parameters, {this.modifiers = const {}});
 
   @override
   String visitList(ExpressionList expression) {
@@ -40,8 +73,12 @@ class ApplyVisitor implements ExpressionVisitor<String> {
 
   @override
   String visitSelect(MultipleExpression expression) {
-    final option = parameters[expression.parameter]!.toString();
-    return expression.options[option]!.visit(this);
+    final modifier = modifiers[expression.modifier];
+    if (modifier != null) {
+      return modifier.resolve(expression, this);
+    }
+    final parameter = parameters[expression.parameter]!.toString();
+    return expression.options[parameter]!.visit(this);
   }
 }
 
