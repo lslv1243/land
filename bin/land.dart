bc2784a995a9ef2e5a7ba06812d9866ef775d6af
Leonardo da Silva <lslv1243@gmail.com>
2022-03-19 13:29:20 -0300

    testing multiple languages

--- bin/land.dart
+++ bin/land.dart
@@ -3,32 +3,50 @@ import 'dart:io';
 import 'package:dart_style/dart_style.dart';
 
 import 'create_language_declaration.dart';
+import 'package:land/generated/l10n.dart';
 
 void main(List<String> arguments) async {
-  final fields = <String, List<String>>{
-    'helloWorld': [],
-    'count': ['count'],
-    'nDogs': ['count'],
-    'iHaveNDogs': ['count'],
-  };
+  // final fields = <String, List<String>>{
+  //   'helloWorld': [],
+  //   'count': ['count'],
+  //   'nDogs': ['count'],
+  //   'iHaveNDogs': ['count'],
+  // };
 
-  final messagesPtBr = <String, String>{
-    'helloWorld': 'Hello World!',
-    'count': '{count}',
-    'nDogs': '{count,plural, zero{No dogs} =1{One dog} other{{count} dogs}}',
-    'iHaveNDogs':
-        'I have {count,plural, zero{no dogs} =1{one dog} other{{count} dogs}}.',
-  };
+  // final messagesEn = <String, String>{
+  //   'helloWorld': 'Hello World!',
+  //   'count': '{count}',
+  //   'nDogs': '{count,plural, zero{No dogs} =1{One dog} other{{count} dogs}}',
+  //   'iHaveNDogs':
+  //       'I have {count,plural, zero{no dogs} =1{one dog} other{{count} dogs}}.',
+  // };
 
-  final files = createDeclarationFiles(
-    fields: fields,
-    locales: {
-      'pt_BR': messagesPtBr,
-    },
-    generateProxy: true,
-  );
+  // final messagesPt = <String, String>{
+  //   'helloWorld': 'Olá mundo!',
+  //   'count': '{count}',
+  //   'nDogs':
+  //       '{count,plural, zero{Nenhum cachorro} =1{Um cachorro} other{{count} cachorros}}',
+  //   'iHaveNDogs':
+  //       'Eu tenho {count,plural, zero{nenhum cachorro} =1{um cachorro} other{{count} cachorros}}.',
+  // };
 
-  await formatAndWriteFiles(files);
+  // final files = createDeclarationFiles(
+  //   fields: fields,
+  //   locales: {
+  //     'en': messagesEn,
+  //     'pt': messagesPt,
+  //   },
+  //   generateProxy: true,
+  // );
+
+  // await formatAndWriteFiles(files);
+
+  final l10nPt = L10NPt();
+  final l10nEn = L10NEn();
+  final proxy = ProxyL10N(l10nPt);
+  print(proxy.helloWorld);
+  proxy.proxy = l10nEn;
+  print(proxy.helloWorld);
 }
 
 Future<void> formatAndWriteFiles(List<LanguageFile> files) async {
